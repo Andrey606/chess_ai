9e07bf175d273e1fff84752c8cab72b1987f7dc7
Andrii <andrey@lucalabs.com>
2022-05-01 18:00:31 +0300

    second commit

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import cv2
 import numpy as np
 import math
+from enum import Enum
+
+class Chess(Enum):
+    PAWN = 1,
+    HORSE = 2,
+    KING = 3,
+    ROOK = 4,
+    BISHOP = 5,
+    QUEEN = 6
+
+
+class ChessColor(Enum):
+    WHITE = 1,
+    BLACK = 2
+
 
 desk_img_path = 'images/desk2.png'
 prefix_path = 'images/chess/'
-black_chess_piece_arr = ['black_pawn.png',
-                         'black_horse.png',
-                         'black_king.png',
-                         'black_rook.png',
-                         'black_bishop.png',
-                         'black_queen.png']  # threshold = 0.8
-white_chess_piece_arr = ['white_pawn.png',
-                         'white_horse.png',
-                         'white_rook.png',
-                         'white_bishop.png',
-                         'white_queen.png',
-                         'white_king.png']  # threshold = 0.4
+black_chess_piece_arr = [{'path': 'black_pawn.png', 'color': ChessColor.BLACK, 'model': Chess.PAWN},
+                         {'path': 'black_horse.png', 'color': ChessColor.BLACK, 'model': Chess.HORSE},
+                         {'path': 'black_king.png', 'color': ChessColor.BLACK, 'model': Chess.KING},
+                         {'path': 'black_rook.png', 'color': ChessColor.BLACK, 'model': Chess.ROOK},
+                         {'path': 'black_bishop.png', 'color': ChessColor.BLACK, 'model': Chess.BISHOP},
+                         {'path': 'black_queen.png', 'color': ChessColor.BLACK, 'model': Chess.QUEEN}]  # threshold = 0.8
+white_chess_piece_arr = [{'path': 'white_pawn.png', 'color': ChessColor.WHITE, 'model': Chess.PAWN},
+                         {'path': 'white_horse.png', 'color': ChessColor.WHITE, 'model': Chess.HORSE},
+                         {'path': 'white_king.png', 'color': ChessColor.WHITE, 'model': Chess.KING},
+                         {'path': 'white_rook.png', 'color': ChessColor.WHITE, 'model': Chess.ROOK},
+                         {'path': 'white_bishop.png', 'color': ChessColor.WHITE, 'model': Chess.BISHOP},
+                         {'path': 'white_queen.png', 'color': ChessColor.WHITE, 'model': Chess.QUEEN}]  # threshold = 0.6
 
 
 def get_img_rect(image_file):
@@ -32,7 +47,7 @@ def convert_to_black_white(image_file):
 
 
 def fix_duplication(loc_arr):
-    threshold = 15
+    threshold = 100
     indexes_to_removing = []
     index = 0
     prev_rect = [0, 0]
@@ -57,9 +72,9 @@ def fix_duplication(loc_arr):
 
 
 def find_chess_piece_position(origin_desk, chess_piece_img, desk_img, threshold):
-    desk_black_white = convert_to_black_white(chess_piece_img)
+    desk_black_white = convert_to_black_white(prefix_path + chess_piece_img['path'])
     chess_piece_black_white_template = convert_to_black_white(desk_img)
-    img_rect = get_img_rect(chess_piece_img)
+    img_rect = get_img_rect(prefix_path + chess_piece_img['path'])
 
     res = cv2.matchTemplate(desk_black_white, chess_piece_black_white_template, cv2.TM_CCOEFF_NORMED)
 
@@ -67,10 +82,14 @@ def find_chess_piece_position(origin_desk, chess_piece_img, desk_img, threshold)
 
     fixed_loc = fix_duplication(loc)
 
+    chess_piece_positions = []
     for pt in zip(*fixed_loc[::-1]):
+        chess_piece_positions.append({'position': pt, 'color': chess_piece_img['color'], 'model': chess_piece_img['model']})
         cv2.rectangle(origin_desk, pt, (pt[0] + img_rect[0], pt[1] + img_rect[1]), (255, 0, 0), 1)
-        chess_name = chess_piece_img.replace(prefix_path, '').replace('.png', '')
-        cv2.putText(origin_desk, chess_name, (pt[0]-10, pt[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0))
+        chess_name = chess_piece_img['path'].replace(prefix_path, '').replace('.png', '')
+        cv2.putText(origin_desk, chess_name, (pt[0], pt[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0))
+
+    return chess_piece_positions
 
 
 def find_rect(origin_desk, image_file):
@@ -88,19 +107,13 @@ def find_rect(origin_desk, image_file):
     # draw all contours in green and accepted ones in red
     contours = cv2.findContours(desk_black_white, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     contours = contours[0] if len(contours) == 2 else contours[1]
-    # area_thresh = 0
-    min_area = 0.95 * 180 * 35
-    max_area = 1.05 * 180 * 35
     result = img.copy()
-
-    counter = []
 
     for c in contours:
         area = cv2.contourArea(c)
         # cv2.drawContours(result, [c], -1, (0, 255, 0), 4)
         # 1912589.0
         if area > 1000000 and area < 1800000:
-            counter.append(area)
             # cv2.drawContours(result, [c], -1, (0, 0, 255), 4)
             min_x = 1000000
             min_y = 1000000
@@ -116,23 +129,28 @@ def find_rect(origin_desk, image_file):
                 if i[0][1] > max_y:
                     max_y = i[0][1]
 
-    cv2.rectangle(origin_desk, (min_x, min_y), (max_x, max_y), (0, 0, 255), 5)
+    cv2.rectangle(origin_desk, (min_x, min_y), (max_x, max_y), (0, 0, 255), 4)
     desk_name = desk_img_path.replace('.png', '')
-    cv2.putText(origin_desk, desk_name, (min_x - 15, min_y - 15), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255))
+    cv2.putText(origin_desk, desk_name, (min_x, min_y - 15), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255))
 
     return [[min_x, min_y], [max_x, max_y]]
 
 
+def find_chess_positions(origin_desk, desk_coord, chess_coord):
+    print()
+
 def main():
     desk = cv2.imread(desk_img_path)
 
-    desk_coord = find_rect(desk, desk_img_path)
-
+    chess_coord = []
     for black_chess_piece in black_chess_piece_arr:
-        find_chess_piece_position(desk, prefix_path + black_chess_piece, desk_img_path, 0.8)
+        chess_coord += find_chess_piece_position(desk, black_chess_piece, desk_img_path, 0.8)
 
     for white_chess_piece in white_chess_piece_arr:
-        find_chess_piece_position(desk, prefix_path + white_chess_piece, desk_img_path, 0.4)
+        chess_coord += find_chess_piece_position(desk, white_chess_piece, desk_img_path, 0.6)
+
+    desk_coord = find_rect(desk, desk_img_path)
+    find_chess_positions(desk, desk_coord, chess_coord)
 
     cv2.imshow('detected', desk)
     cv2.waitKey(0)
